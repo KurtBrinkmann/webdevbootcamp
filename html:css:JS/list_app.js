2e0556e2032d16ed0625e90d6ad82ead9bc4cf0a
Kurt Brinkmann <kurt.brinkmann92@gmail.com>
2022-02-20 20:35:40 -0500

    javaScript1
    notes on javascript

---
+++ html:css:JS/list_app.js
@@ -0,0 +1,58 @@
+console.log("Kurt B")
+
+
+//const password = prompt("Enter your password");
+//if (password.length >= 6 && password.indexOf(' ') === 0) {
+//    console.log("VALID PASSWORD!")
+// }
+// else {
+//    console.log("INCORRECT FORMAT FOR PASSWORD!")
+// }
+
+
+//loop format
+// for (let i = 25; i >= 0; i -= 5) {
+//    console.log(i);
+// }
+
+
+// nested arrays
+
+// const seatingChart = [
+//    ['Kristen', 'Erik', 'Namita'],
+//    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//    ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//    const row = seatingChart[i];
+//    console.log(`ROW #${i + 1}`) 
+//    for (let j = 0; j < row.length; j++) {
+//        console.log(row[j])
+//    }
+// }
+
+
+let input = prompt("What would you like to do?");
+const todos = ['Collect Eggs', 'Go to bank'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('List')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+    }
+    else if (input === 'new') {
+        const newTodo = prompt('Ok, please list task');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`);
+    }
+    else if (input === 'delete') {
+        const index = prompt('Enter Index to delete:');
+        const deleted = todos.splice(index, 1);
+        console.log(`${deleted} has been deleted`);
+    }
+
+    input = prompt("What would you like to do?")
+}
+console.log("Ok you quit the App!") 
